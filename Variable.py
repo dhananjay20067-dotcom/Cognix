b73428197755b558e7a3c61a8ffb27fe84925153
Dhananjay Kamble <dhananjay20067@gmail.com>
2025-10-26 20:28:14 +0530

    Day 5: Functions - parameters, return, multiple functions

---
+++ Variable.py
@@ -0,0 +1,27 @@
+# arithmetic vairables
+
+a = 398
+
+b = 943
+
+c = (a + b)
+
+print (c)
+
+# Assignmennt Operators
+a = 7-2
+print (a)
+b = 8
+b+= 9
+print (b)
+
+# Comparison Operators
+d = (7>9)
+print (d)
+
+a = input("Enter the number a")
+
+b = input("Enter the number b")
+
+print("Number a is: ", a)
+print("Number b is: ", b)
