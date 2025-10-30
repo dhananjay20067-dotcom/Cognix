9b7c203114ab50170be2661e2778163c1ef60f40
Dhananjay Kamble <dhananjay20067@gmail.com>
2025-10-30 18:05:36 +0530

    day4_functions

---
+++ day3_conditional.py
@@ -0,0 +1,68 @@
+# temperature = 34
+
+# if temperature > 30:
+#     print("It's warm")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("Its's  nice")
+# else:
+#   print("Its's cold")
+# print("Done")
+
+# age = 22
+# if age >= 18:
+#    print("Eligible")
+# else:
+#    print("Not Eligible")
+
+# age = 43
+# message = "Eligible" if age>= 18 else "Not eligible"
+# print(message)
+
+# age = 22
+# if age >= 18 and age < 65:
+#    if 18 <= age < 65:
+#     print("Eligible")
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i+=1
+
+# i = 100
+# while i >= 1: #Stopping condition
+#     print(i)
+#     i-=1
+
+# n = int(input("Enter number: "))
+# i = 1
+# while i<= 10:
+#     print(n * i)
+#     i += 1
+
+nums = [1,4,9,16,25,36,49,64,81,100]
+
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if (i == 3):
+#         break
+#     i+= 1
+
+# i  = 0
+# while i <= 5:
+#     if (i == 3):
+#         i+= 1
+#         continue #skips the current iteration
+#     print(i)
+#     i+= 1
+
+#For Loop 
+veggies = {"potato", "brinjal", "carrot", "cabbage"}
+
+for val in veggies:
+    print(val)
